d5e038626af5b146dbf0655b2ed5a407f5f56444
RomanLikhachev <romkin63@gmail.com>
2018-05-05 20:36:26 +0400

    was made usr interface and logIn in vk account

---
+++ app/src/main/java/com/foxystudios/com/testaplication/Aboutof.java
@@ -0,0 +1,15 @@
+package com.foxystudios.com.testaplication;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Aboutof extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceStates){
+        super.onCreate(savedInstanceStates);
+
+        setContentView(R.layout.about_of);
+    }
+
+}
---
+++ app/src/main/java/com/foxystudios/com/testaplication/MainActivity.java
@@ -0,0 +1,165 @@
+package com.foxystudios.com.testaplication;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.methods.VKApiUsers;
+import com.vk.sdk.api.model.VKApiUser;
+import com.vk.sdk.api.model.VKList;
+import com.vk.sdk.api.model.VkAudioArray;
+import com.vk.sdk.api.photo.VKUploadAlbumPhotoRequest;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+            Intent intent = new Intent(MainActivity.this, Aboutof.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_share) {
+            VKSdk.login(this, scope);
+            VkRequest();
+        } else if (id == R.id.nav_send) {
+            this.finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private static final String[] scope = new String[]{
+            VKScope.MESSAGES,
+            VKScope.FRIENDS,
+            VKScope.AUDIO,
+            VKScope.NOHTTPS,
+            VKScope.GROUPS
+    };
+
+    private void VkRequest() {
+        VKRequest request = VKApi.users().get(VKParameters.from(VKApiConst.FIELDS, "first_name", "last_name", "photo_100"));
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                VKApiUser user = ((VKList<VKApiUser>) response.parsedModel).get(0);
+                String usersname = user.first_name;
+                String userslastname= user.last_name;
+                //String userphoto = user.photo_50;
+                TextView nameusr = findViewById(R.id.name);
+                TextView lastnameusr = findViewById(R.id.lastname);
+
+                nameusr.setText(usersname);
+                lastnameusr.setText(userslastname);
+
+            }
+
+            @Override
+            public void onError(VKError error) {}
+            @Override
+            public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {}
+        });
+    }
+    public void photobyusr(){
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Toast.makeText(getApplicationContext(),"Conguration!",Toast.LENGTH_LONG);
+            }
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext(),"Error!",Toast.LENGTH_LONG);
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+}
